0d0162052ae1a2bf98cb8a9b5c30e1ce1d73be02
AmetureC <alejandroflores192@gmail.com>
2026-09-13 10:51:56 -0700

    Delete CartItem.java

--- CartItem.java
+++
@@ -1,9 +0,0 @@
-package com.coloradopizza;
-
-import java.util.List;
-
-public record CartItem(String pizzaName, String size, List<String> toppings, double unitPrice, int quantity) {
-    public double total() {
-        return unitPrice * quantity;
-    }
-}
